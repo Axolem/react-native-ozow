d463155e872d9a35610ca6e978fcf02b7bc0b9fc
Axole Maranjana <axolemaranjana4@gmail.com>
2023-10-23 07:15:46 +0200

    version 0.0.1

--- App.tsx
+++ App.tsx
@@ -1,52 +1,51 @@
 import { useState } from "react";
 import {
   View,
-  Modal,
-  Button,
-  StyleSheet,
-  Linking,
   Text,
+  Button,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Ozow, PaymentLink } from "./src/packages/react-native-ozow";
+import { Ozow } from "react-native-ozow";
 
 export default function App() {
-  const [link, setLink] = useState("");
+  //const [link, setLink] = useState("");
 
-const myLink = new PaymentLink("9219958c9f524a3da49fe518abb0de0b");
+  //const myLink = new PaymentLink("");
 
   return (
     <>
       <StatusBar style="auto" />
 
-      <View style={{paddingTop: 100}}>
+      {/* <View style={{ paddingTop: 100 }}>
         <Button
           title="Pay with Ozow"
           onPress={async () => {
-          const results = await myLink.generateLink({
-            SiteCode: "IPR-IPR-003",
-            CountryCode: "ZA",
-            CurrencyCode: "ZAR",
-            Amount: 1,
-            TransactionReference: "1234567",
-            BankReference: "123456",
-            CancelUrl: "https://www.ozow.com",
-            ErrorUrl: "https://www.ozow.com",
-            SuccessUrl: "https://www.ozow.com",
-            NotifyUrl: "https://www.ozow.com",
-            IsTest: false,
-            Customer: "John Doe",
-          });
-          console.log(results.paymentRequestId, results.success);
-          
+            const results = await myLink.generateLink({
+              SiteCode: "IPR-IPR-003",
+              CountryCode: "ZA",
+              CurrencyCode: "ZAR",
+              Amount: 1,
+              TransactionReference: "1234567",
+              BankReference: "123456",
+              CancelUrl: "https://www.ozow.com",
+              ErrorUrl: "https://www.ozow.com",
+              SuccessUrl: "https://www.ozow.com",
+              NotifyUrl: "https://www.ozow.com",
+              IsTest: false,
+              Customer: "John Doe",
+            });
+            console.log(results.paymentRequestId, results.success);
+
+            setLink(results.url);
           }}
         />
 
+        <Text>{null === link}</Text>
         <Text>{link}</Text>
-      </View>
+      </View> */}
 
 
-      {/* <Ozow
+      <Ozow
         data={{
           SiteCode: "IPR-IPR-003",
           CountryCode: "ZA",
@@ -58,13 +57,13 @@ const myLink = new PaymentLink("9219958c9f524a3da49fe518abb0de0b");
           ErrorUrl: "https://www.ozow.com",
           SuccessUrl: "https://www.ozow.com",
           NotifyUrl: "https://www.ozow.com",
-          IsTest: false,
+          IsTest: true,
           Customer: "John Doe",
         }}
-        privateKey="f276b028558946308361979e4bf88ffa"
+        privateKey=""
         onErrorMessage={(error) => {
-          console.log("Payment Error: ", 
-         // error.description
+          console.log("Payment Error: ",
+            // error.description
           );
 
         }}
@@ -81,7 +80,7 @@ const myLink = new PaymentLink("9219958c9f524a3da49fe518abb0de0b");
         }}
 
 
-      /> */}
+      />
     </>
   );
 }
