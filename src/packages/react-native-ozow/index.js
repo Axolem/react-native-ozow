883a4a0b724193059779049f9d6877a1d111eadd
Axole Maranjana <axolemaranjana4@gmail.com>
2023-11-01 18:16:18 +0200

    clen up

--- src/packages/react-native-ozow/index.js
+++
@@ -1,4 +0,0 @@
-import Ozow from './src/Ozow';
-import PaymentLink from './src/PaymentLink';
-import { CountryCodes, CurrencyCodes, OzowTransactionStatus } from './src/utils/interfaces';
-export { CurrencyCodes, CountryCodes, Ozow, OzowTransactionStatus, PaymentLink };
