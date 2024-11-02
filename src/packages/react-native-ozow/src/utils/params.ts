393cf49ea49cc3aa44a8532691a2b6b85ee4a8e4
Axole Maranajana <axolemar@gmail.com>
2024-11-02 19:27:44 +0200

    fix: update tsup version and improve parameter handling in getSearchParams function

--- src/packages/react-native-ozow/src/utils/params.ts
+++ src/packages/react-native-ozow/src/utils/params.ts
@@ -1,14 +1,14 @@
 import { OzowPaymentResponse } from "./interfaces";
 
 function getSearchParams(urlArr: string[]): OzowPaymentResponse {
-    const params = urlArr[1].split('&');
-    const obj: OzowPaymentResponse = {} as OzowPaymentResponse;
+	const params = urlArr?.[1]?.split("&");
+	const obj: OzowPaymentResponse = {} as OzowPaymentResponse;
 
-    params.forEach((param) => {
-        const [key, value] = param.split('=');
-        obj[key as keyof OzowPaymentResponse] = value;
-    });
-    return obj;
+	params?.forEach((param) => {
+		const [key, value] = param.split("=");
+		obj[key as keyof OzowPaymentResponse] = value!;
+	});
+	return obj;
 }
 
-export default getSearchParams
+export default getSearchParams;
